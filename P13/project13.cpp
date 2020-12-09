3c9f32165c0a6dbc16089bb9d15911e63641155b
Michael Heston <m45t3rhest@gmail.com>
2020-12-09 14:24:00 -0800

    Wrote Board class
    Implimented variables and fuctions allowing manipulation of board.
    Can now revert changes done to board. All functions have safety
    checks in place to reduce bugs.
    
    
    Added function prototypes needed to solve a sudoku puzzle.

--- P13/project13.cpp
+++ P13/project13.cpp
@@ -30,7 +30,116 @@ using namespace std;
 bool debug = true;
 
 class Board {
-
+private:
+   struct entry {
+      struct sqr {
+         pair<int, int> loc;
+         array<bool, 9> poss;
+         int val = 0;
+      };
+   vector<sqr> list;
+   void add(pair<int,int> l, array<bool, 9> p, int v){
+      sqr temp;
+      temp.loc = l;
+      temp.poss = p;
+      temp.val = v;
+      list.push_back(temp);
+   }
+   };
+   
+   array<array<int, 9>, 9> board; //will be filled with file.
+   array<array<array<bool, 9>, 9>, 9> pboard;
+   vector<entry> history;
+   
+   void addToEntry(int row, int col){
+      pair<int, int> l = {row, col};
+      history.back().add(l, pboard[row][col], board[row][col]);
+   }
+   
+public:
+   
+   Board(array<array<int, 9>, 9> b){
+      newMove(); //Makes first entry for history.
+      for(int i = 0; i < 9; i++){
+         for(int ii = 0; ii < 9; ii++){
+            pboard[i][ii].fill(false);
+         }
+      }
+      board = b;
+   }
+   
+   void newMove(){
+      if(history.empty() || !history.back().list.empty()){
+         entry newE;
+         history.push_back(newE);
+      }
+   }
+   
+   void add(int row, int col, int val){
+      addToEntry(row, col);
+      if(board[row][col] != 0){
+         cout << "You can not add a possible number to a filled square\n";
+         return;
+      }
+      
+      pboard[row][col][val - 1] = true;
+   }
+   
+   void remove(int row, int col, int val){
+      addToEntry(row, col);
+      if(board[row][col] != 0){
+         cout << "You can not remove a possible number from a filled square\n";
+         return;
+      }
+      pboard[row][col][val - 1] = false;
+   }
+   
+   array<bool, 10> get(int row, int col){
+      array<bool, 10> temp;
+      if(board[row][col] != 0){
+         temp.fill(false);
+         temp[board[row][col]] = true;
+         temp[0] = true;
+      }else{
+         temp[0] = false;
+         for(int i = 1; i < 10; i++){
+            temp[i] = pboard[row][col][i - 1];
+         }
+      }
+      return temp;
+   }
+   
+   void set(int row, int col, int val){
+      addToEntry(row, col);
+      if(board[row][col] != 0){
+         cout << "You can set a final value for a filled square\n";
+         return;
+      }
+      pboard[row][col] = val;
+   }
+   
+   void goBack(int num = 1){
+      
+      while(history.back().list.empty() || !history.empty()){
+         history.pop_back();
+      }
+      
+      if(num > (int)history.size()){
+         cout << "The size of history is " << history.size()
+               << " and you want to go back " << num
+               << " ,so I will undo all changes.\n";
+         num = history.size();
+      }
+      
+      for(int i = 0; i < num; i++){
+         entry curEntry = history.back();
+         history.pop_back();
+         for(int ii = 0; ii < (int)curEntry.list.size(); ii++){
+            board[curEntry.list[ii].loc.first][curEntry.list[ii].loc.second] = curEntry.list[ii].val;
+            pboard[curEntry.list[ii].loc.first][curEntry.list[ii].loc.second] = curEntry.list[ii].poss;
+         }
+      }
+   }
 };
 
 array<array<int, 9>, 9> readFile();
@@ -47,7 +156,7 @@ void findSingles(Board&);
  * This is the entry point for the program.
  ***********************************************************************/
 int main(){
-   
+   Board myBoard(readFile());
    
    //First find all possables for each square using
    //basic searches.(checkRow, checkCol, checkSquare)
